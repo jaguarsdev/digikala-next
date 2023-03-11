604e25bc5eaf41946d9e7384d2c14168c90e880a
jagvarsdev <jaguarsdev.com@gmail.com>
2023-03-11 14:14:13 +0330

    fix env

--- src/redux/productStore.ts
+++ src/redux/productStore.ts
@@ -3,7 +3,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 export const GetProduct = createAsyncThunk('product/GetProduct', async () => {
-    const res = await axios.get('http://localhost:3000/api/product')
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/product`)
     return res.data
 })
 
